86c18da1ffb399db8e1c6c2e10f08220f4133820
Fred Suter <suterf@ornl.gov>
2024-09-16 01:45:46 -0400

    another try at finding nlohmann_json

--- conf/cmake/Findnlohmann_json.cmake
+++ conf/cmake/Findnlohmann_json.cmake
@@ -1,45 +1,14 @@
-include(FindPackageHandleStandardArgs)
-
-# Look for the header file.
-find_path(NLOHMANN_JSON_INCLUDE_DIR
-    NAMES
-        nlohmann/json.hpp
-    HINTS
-        "${NLOHMANN_JSON_LOCATION}/include"
-        "$ENV{NLOHMANN_JSON_LOCATION}/include"
-    PATHS
-        "$ENV{PROGRAMFILES}/nlohmann/include"
-        "$ENV{PROGRAMFILES}/nlohmann/json/include"
-        "$ENV{PROGRAMFILES}/nlohmann_json/include"
-        /usr/include/
-        /usr/local/include/
-        /opt/local/include/
-        /sw/include/
-    DOC
-        "The directory where headers files of nlohmann_json resides")
-mark_as_advanced(NLOHMANN_JSON_INCLUDE_DIR)
-
-if (NLOHMANN_JSON_INCLUDE_DIR)
-    # Tease the NLOHMANN_JSON_VERSION numbers from the lib headers
-    include(utils/ParseVersion)
-
-    parse_version(${NLOHMANN_JSON_INCLUDE_DIR} nlohmann/json.hpp NLOHMANN_JSON_VERSION_MAJOR)
-    parse_version(${NLOHMANN_JSON_INCLUDE_DIR} nlohmann/json.hpp NLOHMANN_JSON_VERSION_MINOR)
-    parse_version(${NLOHMANN_JSON_INCLUDE_DIR} nlohmann/json.hpp NLOHMANN_JSON_VERSION_PATCH)
-
-    set(NLOHMANN_JSON_VERSION "${NLOHMANN_JSON_VERSION_MAJOR}.${NLOHMANN_JSON_VERSION_MINOR}.${NLOHMANN_JSON_VERSION_PATCH}")
-endif (NLOHMANN_JSON_INCLUDE_DIR)
-
-find_package_handle_standard_args(nlohmann_json
-    REQUIRED_VARS NLOHMANN_JSON_INCLUDE_DIR
-    VERSION_VAR NLOHMANN_JSON_VERSION)
-
-if (nlohmann_json_FOUND)
-  set(NLOHMANN_JSON_INCLUDE_DIRS ${NLOHMANN_JSON_INCLUDE_DIR})
-
-  if (NOT TARGET nlohmann::json)
-    add_library(nlohmann::json UNKNOWN IMPORTED)
-    set_target_properties(nlohmann::json PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${NLOHMANN_JSON_INCLUDE_DIRS}")
-  endif ()
-endif ()
+if( TARGET nlohmann_json )
+  return()
+endif()
+  
+if(NOT NLOHMANNJSON_ROOT)
+  set(NLOHMANNJSON_ROOT "${PROJECT_SOURCE_DIR}/lib/json")
+endif()
+    
+add_library( nlohmann_json INTERFACE )
+target_include_directories(
+  nlohmann_json
+  INTERFACE
+  ${NLOHMANNJSON_ROOT}/include
+)
