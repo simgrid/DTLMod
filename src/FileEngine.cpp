a0c3084d63391dc491f8ad5191c949fc0338315d
Fred Suter <suterf@ornl.gov>
2025-04-16 14:38:53 -0400

    more comments and debug

--- src/FileEngine.cpp
+++ src/FileEngine.cpp
@@ -19,32 +19,42 @@ XBT_LOG_EXTERNAL_DEFAULT_CATEGORY(dtlmod);
 namespace dtlmod {
 /// \cond EXCLUDE_FROM_DOCUMENTATION
 
+
+// FileEngines require to know where to (virtually) write file. This information is given by fullpath which has the
+// following format: NetZone:FileSystem:PathToDirectory
 FileEngine::FileEngine(const std::string& fullpath, Stream* stream) : Engine(fullpath, stream, Engine::Type::File)
 {
-  XBT_DEBUG("Create a new FileEngine");
+  XBT_DEBUG("Create a new FileEngine writing in %s", fullpath.c_str());
+
+  // Parse fullpath
   std::vector<std::string> tokens;
   boost::split(tokens, fullpath, boost::is_any_of(":"), boost::token_compress_on);
   if (tokens.size() != 3)
     throw IncorrectPathDefinitionException(XBT_THROW_POINT, fullpath);
 
+  // Get the NetZone first
   netzone_ = sg4::Engine::get_instance()->netzone_by_name_or_null(tokens[0]);
   if (not netzone_)
     throw std::invalid_argument("Unknown NetZone named: " + tokens[0]);
 
+  // Get the file system in this NetZone. If no file system with the given name exists, the .at() raises an exception 
   file_system_ = sgfs::FileSystem::get_file_systems_by_netzone(netzone_).at(tokens[1]);
 
+  // Extract the partition from the PathToDirectory 
   partition_ = file_system_->get_partition_for_path_or_null(tokens[2]);
   if (not partition_)
     throw std::invalid_argument("Cannot find a partition for that name: " + tokens[2]);
 
+  // Parse the path within the partition for debug purposes
   auto simplified_path                   = sgfs::PathUtil::simplify_path_string(tokens[2]);
   auto path_at_mp                        = sgfs::PathUtil::path_at_mount_point(simplified_path, partition_->get_name());
   std::tie(working_directory_, dataset_) = sgfs::PathUtil::split_path(path_at_mp);
   XBT_DEBUG("Partition: %s; Working Directory : %s; Data Set: %s", partition_->get_cname(), working_directory_.c_str(),
             dataset_.c_str());
 
+  // If this directory doesn't exist yet, create it 
   if (not file_system_->directory_exists(tokens[2])) {
-    XBT_DEBUG("Creating Directory '%s'", tokens[2].c_str());
+    XBT_DEBUG("Creating Directory '%s' on '%s' partition", path_at_mp.c_str(), partition_->get_cname());
     file_system_->create_directory(tokens[2]);
   }
 }
