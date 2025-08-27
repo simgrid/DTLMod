e59fe9e15159b12fec4df1731e9576848f7e6744
Fred Suter <suterf@ornl.gov>
2025-08-27 09:16:21 -0400

    initiate python bindings

---
+++ src/bindings/python/dtlmod_python.cpp
@@ -0,0 +1,10 @@
+/* Copyright (c) 2025. The SWAT Team. All rights reserved.          */
+
+/* This program is free software; you can redistribute it and/or modify it
+ * under the terms of the license (GNU LGPL) which comes with this package. */
+
+ #include <pybind11/pybind11.h> // Must come before our own stuff
+
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
