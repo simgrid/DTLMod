e4bab50577cb431a57383d34c256fcef022725aa
Fred Suter <suterf@ornl.gov>
2025-08-27 10:10:35 -0400

    bindings for the DTL class

--- src/bindings/python/dtlmod_python.cpp
+++ src/bindings/python/dtlmod_python.cpp
@@ -8,3 +8,66 @@
 #include <pybind11/functional.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+
+#include <dtlmod/DTL.hpp>
+#include <dtlmod/DTLException.hpp>
+#include <dtlmod/Engine.hpp>
+#include <dtlmod/FileEngine.hpp>
+#include <dtlmod/FileTransport.hpp>
+#include <dtlmod/Metadata.hpp>
+#include <dtlmod/StagingEngine.hpp>
+#include <dtlmod/StagingMboxTransport.hpp>
+#include <dtlmod/StagingMqTransport.hpp>
+#include <dtlmod/StagingTransport.hpp>
+#include <dtlmod/Stream.hpp>
+#include <dtlmod/Transport.hpp>
+#include <dtlmod/Variable.hpp>
+
+#include <xbt/log.h>
+
+namespace py = pybind11;
+using dtlmod::DTL;
+using dtlmod::Engine;
+using dtlmod::Stream;
+using dtlmod::Variable;
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(python, "python");
+
+
+namespace {
+
+std::string get_dtlmod_version()
+{
+  int major;
+  int minor;
+  //sg_version_get(&major, &minor, &patch);
+  return simgrid::xbt::string_printf("%i.%i", 0, 1);//major, minor, patch);
+}
+} // namespace
+
+PYBIND11_MODULE(dtlmod, m)
+{
+  m.doc() = "DTLMod userspace API";
+
+  m.attr("dtlmod_version") = get_dtlmod_version();
+
+  py::register_exception<dtlmod::DTLInitBadSenderException>(m, "DTLInitBadSenderException");
+  //TODO add other exceptions (when needed)
+
+  /* Class DTL */
+  py::class_<DTL>(m, "DTL", "Data Transport Layer")
+    .def_static("create", static_cast<void(*)()>(&DTL::create), "Create the DTL")
+    .def_static("create", static_cast<void(*)(const std::string&)>(&DTL::create), py::arg("filename"), 
+        "Create the DTL")
+    .def_static("connect", static_cast<std::shared_ptr<DTL>(*)()>(&DTL::connect), 
+        "Connect an Actor to the DTL")
+    .def_static("disconnect", static_cast<void(*)()>(&DTL::disconnect),
+        "Disconnect an Actor from the DTL")
+    .def_property_readonly("has_active_connections", &DTL::has_active_connections, 
+        "Check whether some simulated actors are currently connected to the DTL")
+    .def("add_stream", &DTL::add_stream, py::arg("name"), "Add a data stream to the DTL")
+    .def_property_readonly("get_all_streams", &DTL::get_all_streams, 
+        "Retrieve all streams declared in the DTL")
+    .def_property_readonly("get_stream_by_name_or_null", &DTL::get_stream_by_name_or_null, 
+        "Retrieve a data stream from the DTL by its name");
+}
