65d22e9748cbee153945f0564432ca98c2a00d5b
Fred Suter <suterf@ornl.gov>
2026-01-05 03:33:48 -0500

    fix test

--- test/dtl_config.cpp
+++ test/dtl_config.cpp
@@ -83,9 +83,9 @@ TEST_F(DTLConfigTest, ConfigFile)
       ASSERT_NO_THROW(engine->close());
 
       XBT_INFO("Get a stream by name from the DTL");
-      ASSERT_NO_THROW(stream = dtl->get_stream_by_name_or_null("Stream2"));
-      XBT_INFO("Try to get a stream by name from the DTL that doesn't exist. Should return nullptr");
-      ASSERT_EQ(dtl->get_stream_by_name_or_null("Unknown Stream"), nullptr);
+      ASSERT_NO_THROW(stream = dtl->get_stream_by_name("Stream2").value());
+      XBT_INFO("Try to get a stream by name from the DTL that doesn't exist. Should return std::nullopt");
+      ASSERT_FALSE(dtl->get_stream_by_name("Unknown Stream").has_value());
       XBT_INFO("Open the stream");
       ASSERT_NO_THROW(engine = stream->open("staging", dtlmod::Stream::Mode::Publish));
       XBT_INFO("Stream 2 is opened (%s, %s)", stream->get_engine_type_str(), stream->get_transport_method_str());
