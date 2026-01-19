bd1087be43c4f9544c4fbf1dc50c8bab1ff36684
Fred Suter <suterf@ornl.gov>
2026-01-19 10:46:51 -0500

    fix ODR violation

--- test/test_util.hpp
+++ test/test_util.hpp
@@ -7,16 +7,17 @@
 #define __DTLMOD_TEST_UTIL_HPP__
 #include <functional>
 
-static void DO_TEST_WITH_FORK(const std::function<void()> &lambda) {
-    pid_t pid = fork();
-    if (pid) {
-        int exit_code;
-        waitpid(pid, &exit_code, 0);
-        ASSERT_EQ(exit_code, 0);
-    } else {
-        lambda();
-        exit(::testing::Test::HasFailure() ? 255 : 0);
-    }
+inline void DO_TEST_WITH_FORK(const std::function<void()>& lambda)
+{
+  pid_t pid = fork();
+  if (pid) {
+    int exit_code;
+    waitpid(pid, &exit_code, 0);
+    ASSERT_EQ(exit_code, 0);
+  } else {
+    lambda();
+    exit(::testing::Test::HasFailure() ? 255 : 0);
+  }
 }
 
 #endif // __DTLMOD_TEST_UTIL_HPP__
